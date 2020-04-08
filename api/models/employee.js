aee2370f28508dcd17050004517a5f776fb11ea4
Balaji KC <balajikcdeveloper@gmail.com>
2020-04-08 18:35:05 +0530

    added comment

--- api/models/employee.js
+++ api/models/employee.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+//change this schema to expenses
 const employeeSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     employeeId: { type: Number, required: true },
